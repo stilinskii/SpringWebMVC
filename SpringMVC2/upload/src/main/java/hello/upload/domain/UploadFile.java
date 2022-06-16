de4d6194fe7e4229c91b986d0b638c1ec58e2433
stilinskii <lhy1031hy@gmail.com>
2022-06-17 03:35:35 +0900

    spring mvc2 file upload

---
+++ SpringMVC2/upload/src/main/java/hello/upload/domain/UploadFile.java
@@ -0,0 +1,18 @@
+package hello.upload.domain;
+
+import lombok.Data;
+
+@Data
+public class UploadFile {
+
+    private String uploadFileName;
+    private String storeFileName;
+
+    public UploadFile() {
+    }
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+}
