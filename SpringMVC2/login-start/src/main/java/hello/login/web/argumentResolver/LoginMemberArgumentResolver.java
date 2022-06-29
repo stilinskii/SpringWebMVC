2cc20deac367494163ddb2463e0aacb7e9ba24b5
stilinskii <lhy1031hy@gmail.com>
2022-06-30 02:50:03 +0900

    make validate process without using spring validation

---
+++ SpringMVC2/login-start/src/main/java/hello/login/web/argumentResolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,39 @@
+package hello.login.web.argumentResolver;
+
+import hello.login.domain.member.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static hello.login.web.SessionConst.LOGIN_MEMBER;
+
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        log.info("supportsParameter 실행");
+
+        boolean hasLogin = parameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getParameterType());
+        return hasLogin && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        log.info("resolveArgument 실행");
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return null;
+        }
+
+        return session.getAttribute(LOGIN_MEMBER);
+    }
+}
