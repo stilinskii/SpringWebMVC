93e4723848706f9624f96ad708531b9a9d4c96fd
stilinskii <lhy1031hy@gmail.com>
2022-07-06 23:59:40 +0900

    validation1 done

--- SpringMVC2/validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ SpringMVC2/validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +27,14 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
+
+    //다른 메서드가 호출될때 같이 먼저 호출됨
+    //validator가 여러개일때는 supports 가 알아서 호출되고 맞는 validator를 찾아서 호출
+//    @InitBinder
+//    public void init(WebDataBinder dataBinder){
+//        dataBinder.addValidators(itemValidator);
+//    }
 
     @GetMapping
     public String items(Model model) {
@@ -151,7 +161,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes) {
         //검증로직 , empty나 공백같은 단순한 기능만 제공
@@ -162,7 +172,7 @@ public class ValidationItemControllerV2 {
 //            bindingResult.rejectValue("item","required");
 //        }
 
-        //바인딩 실패하면 밑으로 안가게. 오류메세지 하나만 출력하기 위함
+        //바인딩 실패하면 밑으로 안가게 위로 올려놓기. 오류메세지 하나만 출력하기 위함
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
@@ -188,6 +198,39 @@ public class ValidationItemControllerV2 {
         }
 
         //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+    //@PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+                     RedirectAttributes redirectAttributes){
+        itemValidator.validate(item, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+        //성공로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes){
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/addForm";
+        }
+        //성공로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
