425bb9c939e5175fd71b4a35e4316be9b69100dd
stilinskii <lhy1031hy@gmail.com>
2022-07-10 17:45:23 +0900

    validation done

--- SpringMVC2/validation-start/src/main/java/hello/itemservice/domain/item/Item.java
+++ SpringMVC2/validation-start/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,29 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 100000", message = "총합이 10000원이 넘도록 입력해주세요")
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class)//수정 요구사항 추가
     private Long id;
+    //빈값 공백 허용X
+//    @NotBlank(groups = {SaveCheck.class,UpdateCheck.class})
     private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+//    @Range(min=1000,max=1000000,groups = {SaveCheck.class,UpdateCheck.class})
     private Integer price;
+
+//    @Max(value = 9999, groups = SaveCheck.class)
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
     private Integer quantity;
 
     public Item() {
---
+++ SpringMVC2/validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,28 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult){
+        log.info("API controller access");
+        if(bindingResult.hasErrors()){
+            log.info("validation error = {}", bindingResult);
+            return bindingResult.getAllErrors();//filed errors and object errors
+        }
+
+        log.info("성공로직 실행");
+        return form;
+    }
+}
